fe663884f2e54e44d5499da6e23c7fd25ca1c7f9
Hesham97 <hesham_magdy97@hotmail.com>
2018-03-29 19:46:00 +0200

    solving errors

--- src/main/java/com/SWII/Controllers/StoreProductsController.java
+++ src/main/java/com/SWII/Controllers/StoreProductsController.java
@@ -11,21 +11,21 @@ import org.springframework.web.bind.annotation.RestController;
 import com.SWII.Entity.OnlineStoreEntity;
 import com.SWII.Entity.Store;
 import com.SWII.Entity.StoreProductsEntity;
-import com.SWII.Services.StoreProductsService;
+import com.SWII.Services.OnlineProductServices;
 
 @RestController
 @RequestMapping(value="/productsinstore")
 public class StoreProductsController {
 
 	@Autowired
-	StoreProductsService storeProductService;
-
+	OnlineProductServices storeProductService;
+	
 	@RequestMapping(value="/{storeId}",method=RequestMethod.GET)
 	public List<StoreProductsEntity> getStoreProducts(@PathVariable("storeId") String storeId ){
 		//@toDo Store ya H
 		Store x = new OnlineStoreEntity();
 		x.setStoreId(storeId);
-		return storeProductService.getStoreProduct(x);
+		return null; //storeProductService.getStoreProduct(x);
 	}
 	
 //	public List<StoreProductsEntity> getNumberOfViews(String storeId){
