c4d7cfb4ec1386bf6bb8763d3745583250da8d2a
Hesham97 <hesham_magdy97@hotmail.com>
2018-05-03 02:26:43 +0200

    solving stat.

--- src/main/java/com/SWII/Actions/AddProduct.java
+++
@@ -1,5 +0,0 @@
-package com.SWII.Actions;
-
-public abstract class AddProduct {
-	
-}
--- src/main/java/com/SWII/Entity/UpdateRemoveHistory.java
+++ src/main/java/com/SWII/Entity/UpdateRemoveHistory.java
@@ -1,7 +1,5 @@
 package com.SWII.Entity;
 
-import java.util.Set;
-
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 
